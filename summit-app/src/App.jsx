d92b1f072c840e70f429056550841ace6244ed59
Douglas Oliveira <100490271+Dougladmo@users.noreply.github.com>
2023-10-23 11:50:44 -0300

    Update App.jsx

--- summit-app/src/App.jsx
+++ summit-app/src/App.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 import Home from "./pages/Home/Home"
-import Reservas from './pages/Reserves/Reserves'
+import Reserves from './pages/Reserves/Reserves'
 
 function App() {
   return (
     <Router>
       <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route path="/reservas" element={<Reservas />} />
+          <Route path="/reservas" element={<Reserves />} />
       </Routes>
     </Router>
   )
